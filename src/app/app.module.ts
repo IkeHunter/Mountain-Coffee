e0c7d3ee8cb648443bb14a20dc2c71ebc1c74c3a
Isaac Hunter <ikehunter5@gmail.com>
2020-12-20 14:45:46 -0500

    added about section to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './home/landing/landing.component';
 import { SectionRoutingComponent } from './home/section-routing/section-routing.component';
+import { SectionAboutComponent } from './home/section-about/section-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LandingComponent,
-    SectionRoutingComponent
+    SectionRoutingComponent,
+    SectionAboutComponent
   ],
   imports: [
     BrowserModule,
